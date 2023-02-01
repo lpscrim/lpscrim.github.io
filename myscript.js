97d0d22f30316c8b899fe3937a72eb616bff9843
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-02-01 13:35:48 +0000

    Updated custom counter.

--- myscript.js
+++ myscript.js
@@ -2,31 +2,48 @@
 
     let secs = Math.floor((new Date() - date) / 1000);
   
+
+    let y = "";
+    let m = "";
+    let d = "";
+    let h = "";
+    let mi = "";
+    let sx = "";
+
     let interval = secs / 31536000;
-  
+
+
     if (interval > 1) {
-      return Math.floor(interval) + " years";
+      y = Math.floor(interval) + " years,";
+      interval = secs %= 31536000;
     }
     interval = secs / 2592000;
     if (interval > 1) {
-      return Math.floor(interval) + " months";
+      m =  Math.floor(interval) + " months,";
+      interval = secs %= 2592000;
     }
     interval = secs / 86400;
     if (interval > 1) {
-      return Math.floor(interval) + " days";
+      d = Math.floor(interval) + " days,";
+      interval = secs %= 86400;
     }
     interval = secs / 3600;
     if (interval > 1) {
-      return Math.floor(interval) + " hours";
+      h = Math.floor(interval) + " hours,";
+      interval = secs %= 3600;
     }
     interval = secs / 60;
     if (interval > 1) {
-      return Math.floor(interval) + " minutes";
+      mi = Math.floor(interval) + " minutes";
+      interval = secs %= 60;
     }
-    return Math.floor(secs) + " seconds";
+      sx = Math.floor(secs) + " seconds";
+
+    return ` ${y} ${m} ${d} ${h} ${mi} and ${sx}`;
+    
   }
 
-    let counter = document.getElementById("counter");
-
-    counter.innerHTML = `I have been coding for... ${timeSince(1656675676000)}.`;
+    let counter = () => document.getElementById("counter").innerHTML = `I have been coding for ${timeSince(1656675676000)}.`;
+    
+    setInterval(counter, 1000);
     
