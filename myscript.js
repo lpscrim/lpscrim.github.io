296669c3cfdd801ab599a2636468f1603607aeda
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-02-01 13:02:36 +0000

    Added js for code and removed js for index

--- myscript.js
+++ myscript.js
@@ -1,17 +1,32 @@
-let artDiv = document.querySelector("#art")
-let photoDiv = document.querySelector("#photo")
-let musicDiv = document.querySelector("#music")
-let artistDiv = document.querySelector("#artist")
+    let timeSince = (date) => {
 
-artDiv.onmouseover = () => {
-    artDiv.querySelector(".container").style.display = "flex";
-}
-photoDiv.onmouseover = () => {
-    photoDiv.querySelector(".container").style.display = "flex";
-}
-musicDiv.onmouseover = () => {
-    musicDiv.querySelector(".container").style.display = "flex";
-}
-artistDiv.onmouseover = () => {
-    artistDiv.querySelector(".container").style.display = "flex";
-}
+    let secs = Math.floor((new Date() - date) / 1000);
+  
+    let interval = secs / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = secs / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = secs / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = secs / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = secs / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(secs) + " seconds";
+  }
+
+    let counter = document.getElementById("counter");
+
+    counter.innerHTML = `I have been coding for... ${timeSince(1656675676000)}.`;
+    
